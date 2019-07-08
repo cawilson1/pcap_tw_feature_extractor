d838fd93fffd294d2665b8f678222a18cc813d9d
cawilson1 <30571659+cawilson1@users.noreply.github.com>
2019-07-08 11:27:09 -0400

    file to read pcap csv for feature/label creation

--- read_pcap_csv.py
+++ read_pcap_csv.py
@@ -22,18 +22,11 @@ def getFeatures(featureFile):
                             sep=',',
                             header=0,
                             nrows = 10000)
-    '''
-    labels = pd.read_csv('labels.csv',
-                         sep=',',
-                         header=0)#ignore first row because matlab writes an irrelevant var name to first row
-        '''
-   # featureNames = pd.read_csv('features.csv',
-    #                     sep=',',
-    #                     header=0)#same as above
+
                          
     print('before conversion to np')
     #featureValnp = np.array(featureVals,dtype="float32")
-    featureValnp = 0
+  #  featureValnp = 0
    # labelsnp = np.array(labels)
     print('after conversion to np')
     featureNamesnp = featureVals.head(0).columns.values
@@ -42,37 +35,6 @@ def getFeatures(featureFile):
     #return featureValnp, labelsnp, featureNamesnp
     return featureVals, featureNamesnp
 
-#print(featureValnp)
-#print(np.shape(featureValnp))
-
-#print(labels)
-#print(featureNames)
-''' 
-def getNames(featureFile):
-    featureVals = pd.read_csv(featureFile,
-                            sep=',',
-                            header=0)
-    
-
-    featureNamesnp = featureVals.head(0).columns.values
-
-    
-    return featureNamesnp'''
-'''
-def chooseFeatures(XFeatures, XFile,yFile):
-    inputData = pd.read_csv(XFile,
-                            sep=',',
-                            usecols=XFeatures,
-                            header=0)
-    outputData = pd.read_csv(yFile,
-                             sep=',',
-                             usecols=[0],
-                             header=0)
-
-    tempAllX = np.array(inputData, dtype="float")
- 
-    return tempAllX, np.array(outputData), inputData.head(0).columns.values
-'''
 
 def getTimeFeature(featureFile):
     print('before read')
@@ -84,17 +46,13 @@ def getTimeFeature(featureFile):
     dropPackets = 0
     timeValues = timeValues['Time']#dunno why i have to do this for this to work
     print(timeValues)
+    
+    
     for i in range(len(timeValues)):
         if not timeValues[i] < 60:
             print('drop packets up to ', i)
             dropPackets = i
             break
-        
-    
-  #  print(timeValues)
-   # timeValues = timeValues[timeValues.index > dropPackets]
-    #timeValues = timeValues.drop([i for i in range(dropPackets)])
- #   print(timeValues)
 
     return dropPackets
 
@@ -102,43 +60,14 @@ def main():
     
     #return feature index of packets to eliminate for time windows longer than current time
     dropPackets = getTimeFeature('CICIDS_Friday-WorkingHours.csv')
-    '''    
-    dropPackets = 0
-    timeValues = timeValues['Time']#dunno why i have to do this for this to work
-    print(timeValues)
-    for i in range(len(timeValues)):
-        print(timeValues[i])
-        if not timeValues[i] < 60:
-            print('drop packets up to ', i)
-            dropPackets = i
-            break
+
+    featureValues, featureNames = getFeatures('CICIDS_Friday-WorkingHours.csv')
+    valuesToLabel = featureValues[featureValues.index > dropPackets]
+
+    valuesToLabel = valuesToLabel.set_index(np.arange(len(valuesToLabel)))#this list corresponds to the values that will actually be labelled. The original list is still necessary to compute features    
         
-    '''  
-    featureValues, featureNames = getFeatures('CICIDS_Friday-WorkingHours.csv')
-    featureValues = featureValues[featureValues.index > dropPackets]
-    #needs work
-    featureValues = featureValues.reindex([i for i in range(len(featureValues))],fill_value=[el for el in featureValues])
-    print(featureValues['Time'])
-    return
-    print(timeValues)
-    timeValues = timeValues[timeValues.index > dropPackets]
-    #timeValues = timeValues.drop([i for i in range(dropPackets)])
-    print(timeValues)
-    return
-    featureValues, featureNames = getFeatures('CICIDS_Friday-WorkingHours.csv')
     print(featureValues)
-    print(featureNames)
-    
-    #timeStrings = featureValues['Time']#pd.DataFrame(index=featureValues, columns=['Time'])
-   # print(timeStrings)
-   # print(timeStrings[1] + timeStrings[2])
-    dropPackets = 0
-    for i in range(len(timeStrings)):
-        if not timeStrings[i] < 60:
-            print('drop packets up to ', i)
-            dropPackets = i
-            break
-    print(timeStrings.__class__)
-    timeStrings = timeStrings.drop([i for i in range(dropPackets)])
-    print(timeStrings)
+    print(valuesToLabel)
+    return valuesToLabel,featureValues
+
 main()
